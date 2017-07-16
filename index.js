ce74f2ff8a4c8362dceede3814de4cfa042389ac
SungJae Kim <jay@sungjaes-mbp.attlocal.net>
2017-07-15 23:33:35 -0500

    Change port number

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ app.get('/results/:pollId', function(req, res){
         });
 });
 
-app.listen(3000, function(){
-    console.log('App started on port 3000');
+var port = Number(process.env.PORT || 3000);
+app.listen(port, function(){
+    console.log('App started on port ' + port);
 });
